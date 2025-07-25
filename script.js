48998cd40e9d179c40704b59c2b121757ce1de3a
Gagan Chandra <2k22.csai.2213352@gmail.com>
2025-07-25 20:45:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+function startChaiLoveStory() {
+  alert("Gagan enters the kitchen... 💛\n\nGas isn't turning on 😅\nYashi walks in wearing a saree 💃\nGagan hugs her from behind 😘\n\nAnd then... the magic begins ☕");
+}
